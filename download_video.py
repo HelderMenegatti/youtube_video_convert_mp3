fbcd8529b3faadbb58e063caabf4ee93f91db557
Helder Menegatti <heldermenegatti720@gmail.com>
2024-05-04 22:34:55 -0300

    HotFix Error handling of disallowed characters and handling of repeated items in the urls list

--- download_video.py
+++ download_video.py
@@ -1,14 +1,37 @@
 import os
+import re
 from pytube import YouTube
 from moviepy.editor import *
 
-urls = []
+urls = [
+# ...
+]
+
+
+def  checks_for_duplicate_items(lista):
+    count = []
+    for item in lista:
+        if item in count:
+            continue
+        else:
+            count.append(item)
+    print(f"-- lista bruta {len(lista)} -- lista tratada {len(count)}")
+    return count
+
+
+def remover_caracteres_nao_permitidos(nome_arquivo):
+    # Define a expressão regular para encontrar caracteres não permitidos
+    regex = r"[\\/:*?\"<>|\n\t]"  # Adicione aqui os caracteres que deseja remover
+    nome_arquivo_sem_caracteres_nao_permitidos = re.sub(regex, "", nome_arquivo)
+    return nome_arquivo_sem_caracteres_nao_permitidos
+
 
 def download_video_and_convert(url):
     try:
         # Baixa o video do Youtube
         yt = YouTube(url)
-        video_title = yt.title
+        video_title = remover_caracteres_nao_permitidos(yt.title)
+        # video_title = yt.title
         video_stream = yt.streams.filter(only_audio=True).first()
         temp_video_filename = f"{video_title}.mp4"
         video_stream.download(filename=temp_video_filename)
@@ -27,7 +50,12 @@ def download_video_and_convert(url):
         print("Ocorreu um erro:", str(e))
 
 
-for url in urls:
-    download_video_and_convert(url)
+def convection_process(urls):
+    validated_urls = checks_for_duplicate_items(urls)
+    for url in validated_urls:
+        download_video_and_convert(url)
 
-print("Lista de urls finalizada!!!")
+    print("Lista de urls finalizada!!!")
+
+
+convection_process(urls)
